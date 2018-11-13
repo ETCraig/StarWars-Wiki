784faf3b8f770b6e0d30c5363348fcdadb89af0e
unknown <ethantcraigmk2@gmail.com>
2018-11-12 18:03:05 -0700

    Phase I of NavBar

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './Home.css';
 
+import Nav from '../NavBar/NavBar';
+
 class Home extends Component {
     render() {
         return(
-            <div>
-                Home
+            <div className='Home-App'>
+            <Nav />
+                <div className='Home-Body'>
+
+                </div>
             </div>
         );
     }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
-import './NaveBar.css';
+import './NavBar.css';
+
+import {Link} from 'react-router-dom';
 
 class NaveBar extends Component {
     render() {
+        
         return(
-            <div>
-
+            <div className='NavBar-App'>
+                <div className='NavBar_Body'>
+                    <h1>STAR WARS Wiki</h1>
+                    <div class="container">
+                        <input type="text" placeholder="Search..." />
+                        <div class="search"></div>
+                    </div>
+                    <div class="select">
+                        <select name="slct" id="slct">
+                            <option>Choose a Category</option>
+                            <option value="Characters">Characters</option>
+                            <option value="Factions">Factions</option>
+                            <option value="Planets">Planets</option>
+                            <option value="Ships">Ships</option>
+                            <option value="Weapons">Weapons</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         );
     }
